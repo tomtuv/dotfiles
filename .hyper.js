8b5bc0411c89aabfadba0d82fe49f4d283438600
Thomas Tuvignon <thomas.tuvignon@ultro.fr>
2022-09-27 12:40:35 +0200

    🔧 Update Hyper config

--- .hyper.js
+++ .hyper.js
@@ -1,10 +1,12 @@
-"use strict";
-
+/**
+ * @see https://hyper.is/#cfg
+ */
 module.exports = {
   config: {
     fontFamily:
       "ui-monospace, SFMono-Regular, SF Mono, Menlo, Consolas, Liberation Mono, monospace",
+    cursorShape: "BEAM",
+    cursorBlink: true,
   },
   plugins: ["hyper-snazzy"],
 };
-//# sourceMappingURL=config-default.js.map
